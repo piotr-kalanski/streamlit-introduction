965777ed83d2d85f685b885db632f3965ad052db
Piotr Kalański <piotr.kalanski@gmail.com>
2024-06-05 19:00:27 +0200

    hello world and basic inputs

---
+++ pages/1_Hello_World.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import streamlit as st
+
+st.title("Hello World")
+
+st.header("Sample data")
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+st.write(df)
+
+st.header("Line chart")
+st.line_chart(data=df, x='first column', y='second column')
---
+++ pages/2_Basic_Inputs.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title("Basic Inputs")
+
+vars = {}
+
+st.header('Form')
+vars['checkbox'] = st.checkbox('yes')
+vars['button'] = st.button('Click')
+vars['radio'] = st.radio('Pick your gender', ['Male', 'Female'])
+vars['selectbox'] = st.selectbox('Pick your gender', ['Male', 'Female'])
+vars['multiselect'] = st.multiselect('choose a planet', ['Jupiter', 'Mars', 'neptune'])
+vars['select_slider'] = st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+vars['slider'] = st.slider('Pick a number', 0.50)
+
+st.header('Your selections')
+st.json(vars)
+
+st.header('More inputs')
+st.write('https://docs.streamlit.io/develop/api-reference/widgets')
